493709270ee3daeb7c955e8c152a70d385dcc5f9
Bhuwan karki <bhuwan.kark@gmail.com>
2019-04-25 20:13:42 +0300

    Update keras-dvc-cnn-pretrained.py

--- keras-dvc-cnn-pretrained.py
+++ keras-dvc-cnn-pretrained.py
@@ -59,7 +59,7 @@ else:
 # in half.  In addition, the validation set consists of 1000 images,
 # and the test set of 22000 images.
 
-datapath = "/wrk/YOURUSERNAME/dogs-vs-cats/train-2000"
+datapath = "/wrk/karkibhu/dogs-vs-cats/train-2000"
 (nimages_train, nimages_validation, nimages_test) = (2000, 1000, 22000)
 
 # ### Data augmentation
