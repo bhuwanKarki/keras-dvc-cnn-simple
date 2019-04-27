64650795a0da5afe7e1f46f28a3682639a2b8f28
Bhuwan karki <bhuwan.kark@gmail.com>
2019-04-27 16:46:36 +0300

    Update keras-dvc-cnn-simple.py

--- keras-dvc-cnn-simple.py
+++ keras-dvc-cnn-simple.py
@@ -150,6 +150,9 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
